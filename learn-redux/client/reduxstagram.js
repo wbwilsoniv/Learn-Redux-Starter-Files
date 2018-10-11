e2a00b5694789a1bf155fc9fcc89496fde6d13cd
wbwilsoniv <wbwilsoniv@gmail.com>
2018-10-11 14:16:42 -0400

    Initial commit. Installed dependencies, imported react, render, and css into reduxstagram.js. Test render function working. Components directory added with blank  main.js, photoGrid.js, and Single.js files

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+// render takes JSX and a "mounting point" => Root
+render(<p>Hi</p>, document.getElementById('root'));
+
