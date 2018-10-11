f3209fe01831b8ef2488a1cc003798e6b5e5e73d
wbwilsoniv <wbwilsoniv@gmail.com>
2018-10-11 15:46:27 -0400

    added reducers folder with comments, index, and posts files

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,9 @@
+// reducer takes in two things:
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+
+export function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+}
