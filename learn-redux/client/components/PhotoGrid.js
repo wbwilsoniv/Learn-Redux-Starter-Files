ef79dcc6ae6fb30cfac74203fd4e8aeaa3f81042
wbwilsoniv <wbwilsoniv@gmail.com>
2018-10-13 15:14:42 -0400

    refactored photo.js

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-            Photo Grid
+                {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post}/>)}
             </div>
         )
     }
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,7 +7,7 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // import components
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
