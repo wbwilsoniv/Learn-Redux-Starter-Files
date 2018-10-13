337e4c3dd1c42e7c731adbe0fe2b29d886ec91ec
wbwilsoniv <wbwilsoniv@gmail.com>
2018-10-13 15:44:33 -0400

    refactored comment component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -5,14 +5,15 @@ import Comments from './Comments';
 const Single = React.createClass({
     render() {
         // index of post
-        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const { postId } = this.props.params;
+        const i = this.props.posts.findIndex((post) => post.code === postId);
         const post = this.props.posts[i];
-        // gets the post
+        const postComments = this.props.comments[postId] || [];
         
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
-                <Comments />
+                <Comments postComments={postComments}/>
             </div>
         )
     }
